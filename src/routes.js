894079431af32af6ad54d98cc54fa39a9e239e7a
Renato <renatomarquesteles@gmail.com>
2019-07-05 16:19:09 -0300

    backend: feature: Listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -31,5 +32,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 // Upload de arquivo
 routes.post('/files', upload.single('file'), FileController.store);
+// Listagem de agenda do prestador
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
