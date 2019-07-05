758e4eb5412c931020829eff6b2f61f05e8c3919
Renato <renatomarquesteles@gmail.com>
2019-07-05 01:57:51 -0300

    backend: feature: Adiciona avatar do usuario

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 // Importando os models
 import User from '../app/models/User';
+import File from '../app/models/File';
 // Importando configurações do banco de dados
 import databaseConfig from '../config/database';
 // Array de models
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -15,7 +16,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
     // Chama o método init de cada model
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
