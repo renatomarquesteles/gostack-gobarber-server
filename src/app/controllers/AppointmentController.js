d1e4034ca3456647a422c99ace43c3540b50baeb
Renato <renatomarquesteles@gmail.com>
2019-07-07 21:50:06 -0300

    feature: Configurando Nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 
 import Appointment from '../models/Appointment';
 import File from '../models/File';
+import Mail from '../../lib/Mail';
 import Notification from '../schemas/Notification';
 import User from '../models/User';
 
@@ -121,7 +122,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -140,6 +149,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Um de seus agendamentos foi cancelado',
+    });
 
     return res.json(appointment);
   }
