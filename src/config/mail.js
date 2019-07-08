3d015b06306b9a7f92bd0e6f471d7e9629f48e3e
Renato <renatomarquesteles@gmail.com>
2019-07-08 01:16:49 -0300

    feature: Variaveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '342e8a1794dfbf',
-    pass: '3fa99192da5072',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
